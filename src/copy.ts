b9d4c2d1f87d0f4bbdffa6674d84299a1aa150ab
Lazar <lazareric.com@gmail.com>
2022-12-05 18:51:04 +0100

    - Updated copy.ts - Bug fix, for recurrsive copy for self referencing objects.

--- src/copy.ts
+++ src/copy.ts
@@ -1,17 +1,26 @@
 
 const isArray = value => Array.isArray(value);
+
 const isObject = value => typeof value === 'object' && !!value && value.constructor === Object;
 
 // It keeps the references of the methods and classes,
 // unlike JSON.stringify usually used for deep simple copy
-const copy = <T extends unknown>(value: T): T => {
-  if (isArray(value)) return (value as any[]).map(item => copy(item)) as T;
+const copy = (value: any, values_?: WeakSet<any>): any => {
+  const values = !values_ ? new WeakSet() : values_;
+
+  // Ref circular value
+  if (values.has(value)) return;
+
+  if (isObject(value) || isArray(value)) values.add(value);
+
+  if (isArray(value)) return (value as any[]).map(item => copy(item, values));
+
   if (isObject(value)) {
     const newValue = {};
 
-    Object.keys(value).forEach(key => newValue[key] = copy(value[key]));
+    Object.keys(value).forEach(key => newValue[key] = copy(value[key], values));
 
-    return newValue as T;
+    return newValue;
   }
 
   return value;
