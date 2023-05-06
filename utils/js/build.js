812ca83fcf824592c4a9da353134df60338a21b4
Lazar Eric <lazareric.com@gmail.com>
2023-05-06 21:02:58 +0200

    m

--- utils/js/build.js
+++ utils/js/build.js
@@ -274,7 +274,7 @@ async function docsUpdateTypes(pathTypes, pathUse, isModules) {
   let valueNew = `\n\n### API\n\n`;
 
   parts.forEach(part => {
-    const partName = (part.match(/(?!type|interface|const|function) [^ \(\)\:]+/i) || [])[0]?.trim();
+    const partName = (part.match(/(?!type|interface|const|function|default function|default class) [^ \(\)\:]+/i) || [])[0]?.trim();
 
     valueNew += `#### ${partName}\n\n\`\`\`ts\n${part.trim()}\n\`\`\`\n\n`;
   });
