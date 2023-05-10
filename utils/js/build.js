55fcd28b2d03a00afb68659cea7ac43d838b2382
Lazar Eric <lazareric.com@gmail.com>
2023-05-10 23:49:43 +0200

    m

--- utils/js/build.js
+++ utils/js/build.js
@@ -245,20 +245,6 @@ const capitalizedCammelCase = value => capitalizeCammelCase(kebabCasetoCammelCas
 async function docsUpdateTypes(pathTypes, pathUse, isModules) {
   let data = await fse.readFile(pathTypes, 'utf8');
 
-  data = data.split('\n')
-    .filter(Boolean)
-    .filter(item => !['import'].some(value => item.indexOf(value) === 0))
-    .map(item => {
-      let value = item;
-
-      if (item.startsWith('declare ')) value = value.slice(8);
-
-      if (item.startsWith('export ')) value = value.slice(7);
-
-      return value;
-    })
-    .join('\n');
-
   let name = (path.parse(pathTypes).name).replace('.d', '').replace(/[\(\):]/gi, '');
 
   name = name.includes('-') ? capitalizedCammelCase(name) : name;
@@ -267,26 +253,68 @@ async function docsUpdateTypes(pathTypes, pathUse, isModules) {
 
   const use = fse.existsSync(usePath) ? await fse.readFile(usePath, 'utf8') : '';
 
-  let values = use?.trim().match(/(?:^|}~)[^~]+(?:$|~{)/ig) || [];
+  let values = use?.trim().match(/(?!^|}~)[^~]+(?!$|~{)/ig) || [];
 
-  const parts = data.match(/((type|const) [^{|\n]+{\n[^}]+};)|((type|const|function) [^\n]+)|((interface|class) [^}]+};?\n)/ig) || [];
+  if (values[0]?.startsWith('#')) values[0] = `#${values[0]}`;
+
+  if (values[values.length - 1]?.endsWith('\n``')) values[values.length - 1] = values[values.length - 1] + '`';
+
+  if (values.length > 0 && values.length <= 2) {
+    const md = (values[0].split(/### API[^~]+(?!$|~])/))[0]?.trim();
+    const api = (values[0].match(/### API[^~]+(?!$|~])/) || [])[0]?.trim();
+
+    values.splice(0, 1, md, api);
+
+    values = values.filter(Boolean);
+  }
+
+  let parts = [];
+
+  data.split('\n').forEach(item => {
+    if ([' ', '}'].includes(item[0])) parts[parts.length - 1] += `\n${item}`;
+    else parts.push(item);
+  });
+
+  parts = parts
+    .filter(Boolean)
+    .map(item => item.replace(/(export|declare) /g, ''))
+    .filter(item => {
+      const partName = (item.replace('default ', '').match(/(?!default|type|interface|const|function|class) [^ \(\)\{\}\<\:]+/i) || [])[0]?.trim();
+
+      return (
+        !item.startsWith('import') &&
+        !!partName
+      );
+    });
 
   let valueNew = `\n\n### API\n\n`;
 
-  parts.forEach(part => {
-    const partName = (part.match(/(?!type|interface|const|function|default function|default class) [^ \(\)\:]+/i) || [])[0]?.trim();
+  parts.forEach(item => {
+    const partName = (item.replace('default ', '').match(/(?!default|type|interface|const|function|class) [^ \(\)\{\}\<\:]+/i) || [])[0]?.trim();
 
-    valueNew += `#### ${partName}\n\n\`\`\`ts\n${part.trim()}\n\`\`\`\n\n`;
+    valueNew += `#### ${partName}\n\n\`\`\`ts\n${item.trim()}\n\`\`\`\n\n`;
   });
 
   // Update values value
+  let added = false;
+
   values = values.map(item => {
-    if (item.includes('# API')) return valueNew;
+    if (item.includes('# API')) {
+      added = true;
+
+      return valueNew;
+    }
 
     return item;
   });
 
-  if (!values.length) values.push(valueNew);
+  if (!added) values.push(valueNew);
+
+  values = values.map(item => {
+    if (item.startsWith('{')) return `~${item}~`;
+
+    return item;
+  });
 
   // Update the file or create it if it doesn't exist
   await fse.writeFile(usePath, values.join('\n'));
