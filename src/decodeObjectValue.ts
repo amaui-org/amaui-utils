9b659177e321037e6c5a3708fdb3c30620fd350f
Lazar Eric <lazareric.com@gmail.com>
2024-03-14 17:48:46 +0100

    m

--- src/decodeObjectValue.ts
+++ src/decodeObjectValue.ts
@@ -1,7 +1,10 @@
+import is from './is';
 import parse from './parse';
 import textToInnerHTML from './textToInnerHTML';
 
 const decodeObjectValue = (value: any) => {
+  if (!is('string', value)) return value;
+
   return parse(textToInnerHTML(value));
 };
 
--- src/encodeObjectValue.ts
+++ src/encodeObjectValue.ts
@@ -1,7 +1,10 @@
 import innerHTMLToText from './innerHTMLToText';
+import is from './is';
 import stringify from './stringify';
 
 const encodeObjectValue = (value: any) => {
+  if (!(is('array', value) || is('object', value) || is('object-like', value))) return value;
+
   return innerHTMLToText(stringify(value));
 };
 
