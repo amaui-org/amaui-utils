c0062c37536d4a7282894c0c1b18aaa2c942bac5
Lazar Eric <lazareric.com@gmail.com>
2024-08-31 21:02:26 +0200

    m

--- src/numberWithCommas.ts
+++ src/numberWithCommas.ts
@@ -1,7 +1,7 @@
 import is from './is';
 import castParam from './castParam';
 
-const numberWithCommas = (value_: string | number, delimiter = ',') => {
+const numberWithCommas = (value_: string | number, delimiter = '.') => {
   const value = castParam(value_);
 
   if (is('number', value)) return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, delimiter);
